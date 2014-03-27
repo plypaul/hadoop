7b67302dafbea19848b430e7e1b025570972df97
Brenden Matthews <brenden@diddyinc.com>
2014-03-27 10:08:56 -0700

    Set -Xms in addition to -Xmx.

--- src/main/java/org/apache/hadoop/mapred/ResourcePolicy.java
+++ src/main/java/org/apache/hadoop/mapred/ResourcePolicy.java
@@ -384,15 +384,15 @@ public class ResourcePolicy {
 
         overrides.set("mapred.child.java.opts",
             scheduler.conf.get("mapred.child.java.opts") +
-                " -XX:+UseG1GC -XX:MaxGCPauseMillis=400 -Xmx" + slotJVMHeap + "m");
+                " -XX:+UseG1GC -XX:MaxGCPauseMillis=400 -Xmx" + slotJVMHeap + "m -Xms" + slotJVMHeap + "m");
 
         overrides.set("mapred.map.child.java.opts",
             scheduler.conf.get("mapred.map.child.java.opts") +
-                " -XX:+UseG1GC -XX:MaxGCPauseMillis=400 -Xmx" + slotJVMHeap + "m");
+                " -XX:+UseG1GC -XX:MaxGCPauseMillis=400 -Xmx" + slotJVMHeap + "m -Xms" + slotJVMHeap + "m");
 
         overrides.set("mapred.reduce.child.java.opts",
             scheduler.conf.get("mapred.reduce.child.java.opts") +
-                " -XX:+UseG1GC -XX:MaxGCPauseMillis=400 -Xmx" + slotJVMHeap + "m");
+                " -XX:+UseG1GC -XX:MaxGCPauseMillis=400 -Xmx" + slotJVMHeap + "m -Xms" + slotJVMHeap + "m");
 
         overrides.setLong("mapred.tasktracker.map.tasks.maximum",
             mapSlots);
